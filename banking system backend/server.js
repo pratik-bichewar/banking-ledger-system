f0bb18c4fa745dec64326fed9656082b0e64d423
pratik-bichewar <pratikbichewar04@gmail.com>
2026-07-25 11:29:32 +0530

    Fix server port for deployment

--- banking system backend/server.js
+++ banking system backend/server.js
@@ -2,8 +2,10 @@ require('dotenv').config();
 const connectDB = require('./src/config/db');
 const app = require('./src/app');
 
+const PORT = process.env.PORT || 3000;
+
 connectDB();
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
